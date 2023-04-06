d81698eaa8e5e308c81ac227c59a84e1e40a244a
Vasileva Masha <Vasilevamasha1982@gmail.com>
2023-04-06 13:32:55 +0300

    Задача2

---
+++ semminar/sem1/two.java
@@ -0,0 +1,21 @@
+package semminar.sem1;
+
+public class two {
+	public static void main(String[] args) {
+		int[] nums = new int[] { 1, 3, 2, 4, 3, 5, 3, 3 };
+		int val = 3;
+
+		for (int i = 0; i < nums.length - 1; ++i) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] == val) {
+					nums[i] = nums[j];
+					nums[j] = val;
+				}
+
+			}
+		}
+		for (int i : nums) {
+			System.out.printf("%d,", i);
+		}
+	}
+}
